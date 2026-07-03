444bbaaf0af3ee9b32588577716de589ec02d86b
Taru Sharma <ps9667279@gmail.com>
2026-07-03 23:39:18 +0530

    Time: 74 ms (78.41%) | Memory: 91.6 MB (94.00%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length, fleet = 0;
+        double fleetTime=0;
+        if (n == 1)
+            return 1;
+        int cars[][] = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> a[0] - b[0]);
+
+        for (int i = n - 1; i >= 0; i--) {
+            double time = (double) (target - cars[i][0]) / cars[i][1];
+            if (time > fleetTime) {
+                fleet++;
+                fleetTime = time;
+            }
+        }
+
+        return fleet;
+    }
+}
