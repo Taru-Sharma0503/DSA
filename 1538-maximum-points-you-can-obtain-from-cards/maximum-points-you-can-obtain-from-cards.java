072364fa92340f506a6e5453c42f0fda049b04b9
Taru Sharma <ps9667279@gmail.com>
2026-07-06 16:22:18 +0530

    Time: 3 ms (33.37%) | Memory: 62 MB (17.20%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length, sum = cardPoints[0], left = 0, right = 0, ans = Integer.MIN_VALUE;
-        int prefixSum[] = new int[n];
+        int n = cardPoints.length, sum = 0, left = 0, right = 0;
+        int ans = Integer.MIN_VALUE, currWindowSum = 0;
 
-        prefixSum[0] = cardPoints[0];
-        for (int i = 1; i < n; i++) {
-            sum += cardPoints[i];
-            prefixSum[i] = prefixSum[i - 1] + cardPoints[i];
-        }
+        for (int num : cardPoints)
+            sum += num;
+
+        if (k == n)
+            return sum;
 
         while (right < n) {
-            int windowSize = right - left + 1;
-            int currWindowSum = left > 0 ? prefixSum[right] - prefixSum[left - 1] : prefixSum[right];
-            if (windowSize > n - k) {
+            currWindowSum += cardPoints[right];
+
+            if (right - left + 1 > n - k) {
+                currWindowSum -= cardPoints[left];
                 left++;
-            } else if (windowSize == n - k) {
-                ans = Math.max(ans, sum - currWindowSum);
-                right++;
-            } else {
-                right++;
             }
+
+            if (right - left + 1 == n - k) {
+                ans = Math.max(ans, sum - currWindowSum);
+            }
+
+            right++;
         }
 
         return ans;
