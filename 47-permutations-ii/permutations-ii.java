dcbd16b038b619bc49c3c597a3f1adb6d840f90d
Taru Sharma <ps9667279@gmail.com>
2026-07-09 20:20:04 +0530

    Time: 29 ms (11.78%) | Memory: 46.7 MB (59.57%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    boolean used[];
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+
+        findPermutation(nums, new ArrayList<>());
+        return ans;
+    }
+
+    public void findPermutation(int[] nums, List<Integer> temp) {
+        if (temp.size() == nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+            findPermutation(nums, temp);
+            temp.remove(temp.size() - 1);
+            findPermutation(nums, temp);
+            used[i] = false;
+        }
+    }
+}
