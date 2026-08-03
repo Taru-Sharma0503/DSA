031fb091221ff1e5266ab525ad6a213d41e9e252
Taru Sharma <ps9667279@gmail.com>
2026-08-03 23:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 119.4 MB (6.88%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        boolean flag = false;
+        int m = mat.length, n = mat[0].length, s = 0, e = n - 1, mid;
+        int largestIdx[] = new int[2];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int element = mat[i][j];
-                flag = false;
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            largestIdx = largestOfCol(mat, mid);
 
-                if (j > 0 && mat[i][j - 1] > element ||
-                        j < n - 1 && mat[i][j + 1] > element ||
-                        i > 0 && mat[i - 1][j] > element ||
-                        i < m - 1 && mat[i + 1][j] > element)
-                    flag = true;
+            int left = (largestIdx[1] == 0) ? -1 : mat[largestIdx[0]][largestIdx[1] - 1];
+            int right = (largestIdx[1] == n - 1) ? -1 : mat[largestIdx[0]][largestIdx[1] + 1];
 
-                if (!flag)
-                    return new int[] { i, j };
-            }
+            if (left < mat[largestIdx[0]][largestIdx[1]] &&
+                right < mat[largestIdx[0]][largestIdx[1]])
+                return new int[] { largestIdx[0], largestIdx[1] };
+
+            else if (left > mat[largestIdx[0]][largestIdx[1]])
+                e = mid - 1;
+            else
+                s = mid + 1;
         }
 
         return new int[] { -1, -1 };
+    }
+
+    public int[] largestOfCol(int mat[][], int col) {
+        int largest = Integer.MIN_VALUE, idx = -1;
+
+        for (int i = 0; i < mat.length; i++) {
+            if (mat[i][col] > largest) {
+                largest = mat[i][col];
+                idx = i;
+            }
+        }
+
+        return new int[] { idx, col };
     }
 }
