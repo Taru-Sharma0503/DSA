2af8bbb9c7655a20e0e0847c0e9a1449b00c1306
Taru Sharma <ps9667279@gmail.com>
2026-08-04 19:03:03 +0530

    Time: 41 ms (9.30%) | Memory: 62.9 MB (5.77%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,20 +1,20 @@
 class StockSpanner {
-    int stack[];
+    int stack[][];
     int top;
 
     public StockSpanner() {
-        stack = new int[10001];
+        stack = new int[10001][2];
         top = -1;
     }
 
     public int next(int price) {
         int ans = 1;
         int currTop = top;
-        while (currTop >= 0 && stack[currTop] <= price) {
-            ans++;
-            currTop--;
+        while (currTop >= 0 && stack[currTop][0] <= price) {
+            ans += stack[currTop][1];
+            currTop -= stack[currTop][1];
         }
-        stack[++top] = price;
+        stack[++top] = new int[] { price, ans };
         return ans;
     }
 }
