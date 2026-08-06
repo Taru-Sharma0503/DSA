5fe5204ddf18076056dbe746b62a1ed11afce5f9
Taru Sharma <ps9667279@gmail.com>
2026-08-06 20:29:29 +0530

    Time: 18 ms (13.33%) | Memory: 69.8 MB (79.22%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,42 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null)
             return null;
 
-        if (root == p && hasChild(root, q))
-            return p;
-
-        if (root == q && hasChild(root, p))
-            return q;
-
-        if ((hasChild(root.left, p) || hasChild(root.left, q)) && (hasChild(root.right, p) || hasChild(root.right, q)))
+        if (root == p || root == q)
             return root;
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
-        if (left != null)
-            return left;
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (right != null)
-            return right;
-        return null;
-    }
 
-    public boolean hasChild(TreeNode root, TreeNode child) {
-        if (root == null)
-            return false;
+        if (left != null && right != null)
+            return root;
 
-        if (root.val == child.val)
-            return true;
-
-        return hasChild(root.left, child) || hasChild(root.right, child);
+        return left != null ? left : right;
     }
 }
