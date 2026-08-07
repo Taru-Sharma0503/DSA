ac784e5fd9b191aa5d514adebb9a7b5e993cccf2
Taru Sharma <ps9667279@gmail.com>
2026-08-07 17:58:34 +0530

    Time: 1 ms (97.31%) | Memory: 46.7 MB (9.53%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,28 +1,35 @@
 class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildSubtree(preorder, inorder);
+
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+
+        return buildSubtree(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
     }
 
-    public TreeNode buildSubtree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0)
+    public TreeNode buildSubtree(int[] preorder, int preStart, int preEnd,
+                                 int[] inorder, int inStart, int inEnd) {
+
+        if (preStart > preEnd)
             return null;
 
-        TreeNode node = new TreeNode(preorder[0]);
+        TreeNode node = new TreeNode(preorder[preStart]);
 
-        if (preorder.length == 1)
-            return node;
+        int idx = map.get(node.val);
 
-        int leftSubtreeSize = 0;
-        while (inorder[leftSubtreeSize] != node.val)
-            leftSubtreeSize++;
+        int leftSubtreeSize = idx - inStart;
 
         node.left = buildSubtree(
-                Arrays.copyOfRange(preorder, 1, leftSubtreeSize + 1),
-                Arrays.copyOfRange(inorder, 0, leftSubtreeSize));
+                preorder, preStart + 1, preStart + leftSubtreeSize,
+                inorder, inStart, idx - 1);
 
         node.right = buildSubtree(
-                Arrays.copyOfRange(preorder, leftSubtreeSize + 1, preorder.length),
-                Arrays.copyOfRange(inorder, leftSubtreeSize + 1, inorder.length));
+                preorder, preStart + leftSubtreeSize + 1, preEnd,
+                inorder, idx + 1, inEnd);
 
         return node;
     }
