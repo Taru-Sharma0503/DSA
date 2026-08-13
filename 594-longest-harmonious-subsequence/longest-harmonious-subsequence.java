78028ef22a92a813bccdc95ea02c39320fb1efe8
Taru Sharma <ps9667279@gmail.com>
2026-08-13 10:06:27 +0530

    Time: 17 ms (40.43%) | Memory: 48.3 MB (25.45%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -2,19 +2,15 @@ class Solution {
     public int findLHS(int[] nums) {
         int n = nums.length, ans = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        HashSet<Integer> set=new HashSet<>();
 
         for (int num : nums)
             map.put(num, map.getOrDefault(num, 0) + 1);
 
         for (int i = 0; i < n; i++) {
-            if (set.contains(nums[i]))
-                continue;
-
             if (map.containsKey(nums[i] + 1)) {
                 int len = map.get(nums[i]) + map.get(nums[i] + 1);
                 ans = Math.max(ans, len);
-                set.add(nums[i]);
+                ;
             }
         }
 
