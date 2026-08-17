8bf9d71c293fc99c0eccea1567139339d8e13571
Taru Sharma <ps9667279@gmail.com>
2026-08-17 10:22:18 +0530

    Time: 2 ms (63.44%) | Memory: 46.5 MB (51.43%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,4 +1,5 @@
 class Solution {
+    int component=0;
 
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length, n = grid[0].length, max = 0;
@@ -7,9 +8,9 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1 && visited[i][j] == 0) {
-                    List<Integer> component = new ArrayList<>();
-                    dfs(grid, i, j, component, visited);
-                    max = Math.max(max, component.size());
+                    component=0;
+                    dfs(grid, i, j, visited);
+                    max = Math.max(max, component);
                 }
             }
         }
@@ -17,20 +18,20 @@ class Solution {
         return max;
     }
 
-    public void dfs(int[][] grid, int i, int j, List<Integer> component, int[][] visited) {
+    public void dfs(int[][] grid, int i, int j, int[][] visited) {
         visited[i][j] = 1;
-        component.add(1);
+        component++;
 
         if (i > 0 && grid[i - 1][j] == 1 && visited[i - 1][j] == 0)
-            dfs(grid, i - 1, j, component, visited);
+            dfs(grid, i - 1, j, visited);
 
         if (j > 0 && grid[i][j - 1] == 1 && visited[i][j - 1] == 0)
-            dfs(grid, i, j - 1, component, visited);
+            dfs(grid, i, j - 1, visited);
 
         if (i < grid.length - 1 && grid[i + 1][j] == 1 && visited[i + 1][j] == 0)
-            dfs(grid, i + 1, j, component, visited);
+            dfs(grid, i + 1, j, visited);
 
         if (j < grid[0].length - 1 && grid[i][j + 1] == 1 && visited[i][j + 1] == 0)
-            dfs(grid, i, j + 1, component, visited);
+            dfs(grid, i, j + 1, visited);
     }
 }
