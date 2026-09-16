d3fc99cd025e4ff26f72df98a5869591057432dc
Taru Sharma <ps9667279@gmail.com>
2026-09-16 22:55:35 +0530

    Time: 3 ms (99.81%) | Memory: 46.3 MB (78.05%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,31 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer,String> map=new HashMap<>();
-        int arr[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        StringBuilder ans=new StringBuilder();
+        String value[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int arr[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        StringBuilder ans = new StringBuilder();
 
-        map.put(1,"I");
-        map.put(5,"V");
-        map.put(10,"X");
-        map.put(50,"L");
-        map.put(100,"C");
-        map.put(500,"D");
-        map.put(1000,"M");
-        map.put(4,"IV");
-        map.put(9,"IX");
-        map.put(40,"XL");
-        map.put(90,"XC");
-        map.put(400,"CD");
-        map.put(900,"CM");
+        while (num > 0) {
 
-        while(num>0){
-
-            for(int val:arr){
-                if(val>num)
+            for (int i=0;i<arr.length;i++) {
+                if (arr[i] > num)
                     continue;
 
-                ans.append(map.get(val));
-                num-=val;
+                ans.append(value[i]);
+                num -= arr[i];
                 break;
             }
         }
