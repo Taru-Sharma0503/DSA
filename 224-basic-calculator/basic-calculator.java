edef0231aa7ed18eb8acdcec0269314f803c469d
Taru Sharma <ps9667279@gmail.com>
2026-09-19 22:48:43 +0530

    Time: 9 ms (92.33%) | Memory: 46.6 MB (25.77%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,114 +1,43 @@
 class Solution {
     public int calculate(String s) {
-        int ans = 0, len = s.length(), num = 0;
-        char operation = '#';
-        Stack<Character> stack = new Stack<>();
 
-        for (int i = len - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
+        Stack<Integer> stack = new Stack<>();
 
-            if (ch == ' ')
-                continue;
+        int res = 0;
+        int curr = 0;
+        int sign = 1;
 
-            if (ch == ')') {
-                stack.push(ch);
-            }
-            else if (ch == '(') {
-                int result = 0;
-                num = 0;
-                char op = '#';
+        for (char c : s.toCharArray()) {
 
-                char character = stack.pop();
-
-                while (character != ')') {
-
-                    if (Character.isDigit(character)) {
-                        num = 0;
-
-                        while (true) {
-                            num = num * 10 + (character - '0');
-
-                            if (stack.isEmpty() || !Character.isDigit(stack.peek()))
-                                break;
-
-                            character = stack.pop();
-                        }
-
-                        if (op == '#')
-                            result = num;
-                        else if (op == '+')
-                            result += num;
-                        else
-                            result -= num;
-
-                        op = '#';
-                    }
-                    else if (character == '+') {
-                        op = '+';
-                    }
-                    else {
-                        // CHANGED: "--" cancels out to "+"
-                        op = (op == '-') ? '+' : '-';
-                    }
-
-                    character = stack.pop();
-                }
-
-                String value = String.valueOf(result);
-
-                for (int j = value.length() - 1; j >= 0; j--)
-                    stack.push(value.charAt(j));
-
-                // Preserve unary minus
-                if (op == '-')
-                    stack.push('-');
-            }
-            else {
-                stack.push(ch);
+            if (Character.isDigit(c)) {
+                curr = curr * 10 + (c - '0');
+            } 
+            else if (c == '+') {
+                res += curr * sign;
+                sign = 1;
+                curr = 0;
+            } 
+            else if (c == '-') {
+                res += curr * sign;
+                sign = -1;
+                curr = 0;
+            } 
+            else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+                curr = 0;
+            } 
+            else if (c == ')') {
+                res += curr * sign;
+                curr = 0;
+                res *= stack.pop();
+                res += stack.pop();
             }
         }
 
-        // CHANGED: haveNumber removed
-
-        while (!stack.isEmpty()) {
-            char ch = stack.pop();
-
-            if (Character.isDigit(ch)) {
-                num = 0;
-
-                while (true) {
-                    num = num * 10 + (ch - '0');
-
-                    if (stack.isEmpty() || !Character.isDigit(stack.peek()))
-                        break;
-
-                    ch = stack.pop();
-                }
-
-                if (operation == '#')
-                    ans = num;
-                else if (operation == '+')
-                    ans += num;
-                else
-                    ans -= num;
-
-                operation = '#';
-
-                if (ch == '+' || ch == '-')
-                    operation = ch;
-            }
-            else if (ch == '+') {
-                operation = '+';
-            }
-            else {
-                // CHANGED: "--" cancels out to "+", and a leading '-' acts as 0 - num
-                operation = (operation == '-') ? '+' : '-';
-            }
-        }
-
-        if (operation == '-')
-            return -ans;
-
-        return ans;
+        res += sign * curr;
+        return res;
     }
 }
